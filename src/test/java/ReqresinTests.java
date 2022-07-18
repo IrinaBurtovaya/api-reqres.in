7498f2a0d5f8ec8b40717fdfe483138ea416d769
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-07-18 21:41:29 +0300

    final changes

--- src/test/java/ReqresinTests.java
+++ src/test/java/ReqresinTests.java
@@ -14,50 +14,95 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ReqresinTests {
 
     @Test
-    @AllureId("11322")
-    @Tag("totalUsersCount")
+    @AllureId("11366")
+    @Tag("totalCount")
     @DisplayName("Проверка общего количества пользователей")
     void checkTotalCountOfUsers() {
-        step("Проверка общего количества пользователей");
+        given()
+                .spec(Specs.request)
+                .get("/users?page=2")
+                .then()
+                .spec(Specs.response)
+                .body("total", is(12));
     }
 
     @Test
-    @AllureId("11323")
+    @AllureId("11367")
     @Tag("specificEmail")
     @DisplayName("Проверка email конкретного пользователя")
     void checkSpecificEmail() {
-        step("Проверка email конкретного пользователя");
+        List<UserData> users = given()
+                .spec(Specs.request)
+                .get("/users?page=2")
+                .then()
+                .spec(Specs.response)
+                .body("total", is(12))
+                .extract().jsonPath().getList("data", UserData.class);
+        assertEquals("byron.fields@reqres.in", users.get(3).getEmail());
     }
 
     @Test
-    @AllureId("11324")
-    @Tag("checkEmailGroovy")
+    @AllureId("11368")
+    @Tag("groovy")
     @DisplayName("Проверки email и имени, используя Groovy")
     void checkEmailUsingGroovy() {
-        step("Проверки email и имени, используя Groovy");
+        given()
+                .spec(Specs.request)
+                .get("/users?page=2")
+                .then()
+                .spec(Specs.response)
+                .body("data.findAll{it.email =~/.*?@reqres.in/}.email.flatten()",
+                        hasItem("byron.fields@reqres.in"))
+                .body("data.findAll{it.first_name}.first_name.flatten()", hasItem("Byron"));
     }
 
     @Test
-    @AllureId("11325")
-    @Tag("createUser")
+    @AllureId("11369")
     @DisplayName("Проверка создания пользователя")
+    @Tag("createUser")
     void createUser() {
-        step("Проверка создания пользователя");
+        String body = "{ \"name\": \"morpheus\", \"job\": \"leader\", \"id\": \"598\", \"createdAt\": \"2022-06-07T11:53:47.580Z\" }";
+
+        given()
+                .spec(Specs.request)
+                .body(body)
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(201)
+                .log().status()
+                .log().body()
+                .body("name", is("morpheus"));
     }
 
     @Test
-    @AllureId("11326")
+    @AllureId("11370")
     @Tag("deleteUser")
     @DisplayName("Проверка успешного удаления пользователя")
     void deleteUserStatusCode204() {
-        step("Проверка успешного удаления пользователя");
+        given()
+                .spec(Specs.request)
+                .when()
+                .delete("/users/2")
+                .then()
+                .statusCode(204);
     }
 
     @Test
-    @AllureId("11327")
+    @AllureId("11371")
     @Tag("unsuccessfulLogin")
     @DisplayName("Проверка неуспешной авторизации")
     void checkUnsuccessfulLogin() {
-        step("Проверка неуспешной авторизации");
+        String body = "{ \"email\": \"peter@klaven\" }";
+        given()
+                .spec(Specs.request)
+                .body(body)
+                .when()
+                .post("/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
     }
 }
